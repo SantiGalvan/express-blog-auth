e54813f15431cc669ae76272b6b12de89a011e9d
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-29 15:47:54 +0200

    added auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,21 @@
+const users = require("../database/users.json");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const generateToken = user => jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "2h" });
+
+const login = (req, res) => {
+    const { username, password } = req.body;
+
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (!user) res.status(404).send('Credenziali non corrette');
+
+    const token = generateToken(user);
+
+    res.send(token);
+}
+
+module.exports = {
+    login
+}
